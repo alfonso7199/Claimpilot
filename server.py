e38daa36b036aa87a0372c8ac1039b8bb8d455df
alfon7199 <alfonso1799@gmail.com>
2026-06-30 18:23:25 +0200

    Prepare project for deployment

--- server.py
+++ server.py
@@ -334,4 +334,5 @@ app.mount("/static", StaticFiles(directory=WEB_DIR), name="static")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=False)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
